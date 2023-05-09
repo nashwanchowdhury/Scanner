ba808f9b1c360ee14f8739769849670fa13383eb
Nashwan Chowdhury <nashwanchowder@gmail.com>
2023-05-09 13:25:40 -0400

    newfiles

---
+++ src/Greeting.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Greeting {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("What's your name?");
+    String response = scanner.nextLine();
+    System.out.println(response);
+
+    }
+    }
